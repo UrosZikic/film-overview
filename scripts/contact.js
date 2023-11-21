e105bb0381c2db996d9967c0dbf32da3915ae894
UrosZikic <uroszikic1512988@gmail.com>
2023-11-21 13:54:37 +0100

    contact extra validation

--- scripts/contact.js
+++ scripts/contact.js
@@ -10,6 +10,40 @@ function sendMail() {
     return; // Exit the function if any field is empty
   }
 
+  // Check for proper email extension
+  if (
+    !document.querySelector("#email").value.includes("@gmail.com") &&
+    !document.querySelector("#email").value.includes("@yahoo.com")
+  ) {
+    alert("Please provide a proper email");
+    return;
+  }
+  // Define a regular expression for valid Gmail or Yahoo addresses
+  const gmailRegex = /^[a-zA-Z0-9._-]+@gmail\.com$/;
+  const yahooRegex = /^[a-zA-Z0-9._-]+@yahoo\.com$/;
+
+  // Check if the email matches the defined patterns
+  if (
+    !(
+      gmailRegex.test(document.querySelector("#email").value) ||
+      yahooRegex.test(document.querySelector("#email").value)
+    )
+  ) {
+    alert("Please enter a valid Gmail or Yahoo address");
+    return; // Exit the function if the email is not Gmail or Yahoo
+  }
+
+  // Check if the email contains both "@gmail.com" and "@yahoo.com"
+  if (
+    document.querySelector("#email").value.includes("@gmail.com") &&
+    document.querySelector("#email").value.includes("@yahoo.com")
+  ) {
+    alert(
+      "Please enter a valid email address. It cannot contain both Gmail and Yahoo domains."
+    );
+    return; // Exit the function if the email contains both Gmail and Yahoo
+  }
+
   (function () {
     emailjs.init("K2PSUkRlxwE8wR98m"); // Replace with your actual user ID
   })();
@@ -40,6 +74,7 @@ function sendMail() {
 
   const formContainer = document.querySelector(".form-container");
   formContainer.classList.add("disappear");
+  document.querySelector("html").style.overflowY = "scroll";
 }
 
 document.querySelector(".contact-button").onclick = () => {
